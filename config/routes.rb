82dc8244656d3f0fa9245d3e69b19562b2ca0a5b
AlexJon93 <alex.jon.jarvis@gmail.com>
2020-04-13 18:18:15 +1000

    created sessions controller and methods to be filled out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: redirect('/login')
+  get '/login', to: 'sessions#new'
   get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
