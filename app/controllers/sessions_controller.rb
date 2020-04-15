d8f6225122f9288d0f4e352b31ccc64dc4e5415d
AlexJon93 <alex.jon.jarvis@gmail.com>
2020-04-15 12:51:50 +1000

    filled out login view, basic functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      flash[:success] = 'login successful!'
+      redirect_to login_path
+    else
+      flash[:danger] = 'Invalid password/username combination'
+      redirect_to login_path
+    end
   end
 
   def destroy
