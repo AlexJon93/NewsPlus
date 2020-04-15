36d24b41d86d215de25cfe6a3731446babe783d1
AlexJon93 <alex.jon.jarvis@gmail.com>
2020-04-15 11:40:08 +1000

    added signup functionality and flash messaging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User created!'
+      redirect_to login_path
+    else
+      flash[:danger] = @user.errors.full_messages
+      redirect_to signup_path
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :mobile, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: redirect('/login')
-  get '/login', to: 'sessions#new'
-  get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get   '/login',   to: 'sessions#new'
+  get   '/signup',  to: 'users#new'
+  post  '/signup',  to: 'users#create'
+  resources :users, only: [:create]
 end
