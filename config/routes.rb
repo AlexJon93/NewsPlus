dae58d66cafe74bd43fa50f5f6d08494e7086179
AlexJon93 <alex.jon.jarvis@gmail.com>
2020-04-18 23:37:49 +1000

    modified navbar view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  get   '/login',   to: 'sessions#new'
-  post  '/login',   to: 'sessions#create'
-  get   '/signup',  to: 'users#new'
-  post  '/signup',  to: 'users#create'
+  root      'posts#index'
+  get       '/login',     to: 'sessions#new'
+  post      '/login',     to: 'sessions#create'
+  get       '/signup',    to: 'users#new'
+  post      '/signup',    to: 'users#create'
+  delete    '/logout',    to: 'sessions#destroy'
   resources :users, only: [:create]
 end
