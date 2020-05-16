57336df1c4dee6838d8ac430b0a66493cf3dbdb5
AlexJon93 <alex.jon.jarvis@gmail.com>
2020-05-16 17:05:16 +1000

    added sidebar to feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,32 @@ class PostsController < ApplicationController
     before_action :check_login, only: [:index]
 
     def index
+        @recent_users = [
+            {
+                username: 'Alex',
+                likes: 140
+            },
+            {
+                username: 'Stan',
+                likes: 148
+            },
+            {
+                username: 'Sarah',
+                likes: 128
+            },
+            {
+                username: 'Guy',
+                likes: 100
+            }
+        ]
+
+        @topics = ['A.I.', 'V.R.', 'RMIT', 'Games', 'Rails', 'Go', 'Web', 'M.L.', 'Joke', 'React', 'Vue', 'Node', 'iOS', 'AWS']
+
+        @popular_posts = [
+            {
+                title: 'MIDI 2.0 Specifications Available for Download',
+                views: 685
+            }
+        ]
     end
 end
