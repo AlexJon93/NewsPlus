c1d1c6d977c91ded34b76b1c44ec5e6d30c5a93e
AlexJon93 <alex.jon.jarvis@gmail.com>
2020-05-16 18:56:26 +1000

    get posts from active record instead of hardcoded data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,28 +2,11 @@ class PostsController < ApplicationController
     before_action :check_login, only: [:index]
 
     def index
-        @recent_users = [
-            {
-                username: 'Alex'
-            },
-            {
-                username: 'Stan'
-            },
-            {
-                username: 'Sarah'
-            },
-            {
-                username: 'Guy'
-            }
-        ]
+        @recent_users = User.order(:last_active_at)
+        @posts = Post.order(:created_at)
 
         @topics = ['A.I.', 'V.R.', 'RMIT', 'Games', 'Rails', 'Go', 'Web', 'M.L.', 'Joke', 'React', 'Vue', 'Node', 'iOS', 'AWS']
 
-        @popular_posts = [
-            {
-                title: 'MIDI 2.0 Specifications Available for Download',
-                views: 685
-            }
-        ]
+        @popular_posts = Post.order(:views)
     end
 end
